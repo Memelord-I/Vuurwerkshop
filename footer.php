b42aa968968c622486bedc2438a85a1ea2156b12
ProjectVUurwerk <57757475+ProjectVUurwerk@users.noreply.github.com>
2019-11-26 13:56:24 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,1 @@
+<div class="footer">&copy Made by the Chorro's</div>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<header>
+<title>Chorro's</title>
+<div class= "header"></div>
+<div class= "winkelwagen"></div>
+
+<div class="topnav">
+  <a href="../index.php">Home</a>
+  <a href="PLAATSDEURLVOORPRODUCTEN">Producten</a>
+  <a href="PLAATSDEURLVOORCONTACT">Contact</a>
+  </div>
+  <div class="winkelmand">
+  <a href="winkelmand/index.php">Winkelwagen</a>
+</div>
+
+</header>
+<link rel="stylesheet" href="css/style.css">
+<body>
+
+
+<?php include 'footer.php';?>
+
+</body>
+</html>
