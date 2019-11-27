ce3a48e92e77a602414536605449a3f927a96cd7
ProjectVUurwerk <57757475+ProjectVUurwerk@users.noreply.github.com>
2019-11-27 10:58:50 +0100

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<header>
+<div class="topnav">
+<nav>
+<ul>
+  <li><a href="index.php">Home</a></li>
+   <li><a href="knalvuurwerk.php">Knalvuurwerk</a>
+   <ul>
+   <li><a href="#">Zacht</a></li>
+   <li><a href="#">Normaal</a></li>
+   <li><a href="knalhard.php">Hard</a></li>
+   </ul>
+   </li>
+    <li><a href="siervuurwerk.php">Siervuurwerk</a>
+	   <ul>
+   <li><a href="#">Fontijnen</a></li>
+   <li><a href="#">Pijlen</a></li>
+   <li><a href="#">Cakes</a></li>
+   </ul>
+   </li>
+	 <li><a href="producten.php">Compleet assortiment</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  </div>
+  <div class="winkelmand">
+  <a href="#">Winkelwagen</a>
+  <link rel="stylesheet" href="css/style.css">
+  </ul>
+  </nav>
+</header>
--- knalhard.php
+++ knalhard.php
@@ -6,33 +6,7 @@
 <div class= "winkelwagen"></div>  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<header>
-<div class="topnav">
-<nav>
-<ul>
-  <li><a href="index.php">Home</a></li>
-   <li><a href="knalvuurwerk.php">Knalvuurwerk</a>
-   <ul>
-   <li><a href="#">Zacht</a></li>
-   <li><a href="#">Normaal</a></li>
-   <li><a href="knalhard.php">Hard</a></li>
-   </ul>
-   </li>
-    <li><a href="siervuurwerk.php">Siervuurwerk</a>
-	   <ul>
-   <li><a href="#">Fontijnen</a></li>
-   <li><a href="#">Pijlen</a></li>
-   <li><a href="#">Cakes</a></li>
-   </ul>
-   </li>
-	 <li><a href="producten.php">Compleet assortiment</a></li>
-  <li><a href="contact.php">Contact</a></li>
-  </div>
-  <div class="winkelmand">
-  <a href="#">Winkelwagen</a></div>
-  </ul>
-  </nav>
-</header>
+<?php include 'includes/header.php'?>
 </div>
 <div id="product-grid">
 	<div class="txt-heading">Producten</div>
